98f6de12b540bcb84097277dc6d1c6d83fa0cfd9
Jake <heaney_jake@outlook.com>
2019-01-07 15:29:17 +0000

    cors add

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const bodyParser = require('body-parser');
 const apiRouter = require('./routers/apiRouter');
 const { handle400, handle404, handle422 } = require('./errors/index');
 const listEndpoints = require('express-list-endpoints');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
-  if (req.url === '/api') {
-    res.status(200).send({ paths: listEndpoints(app) });
-  } else next();
+	if (req.url === '/api') {
+		res.status(200).send({ paths: listEndpoints(app) });
+	} else next();
 });
 
 app.use('/api', apiRouter);
 
 app.use('/*', (req, res, next) => {
-  next({ status: 404, message: 'page not found' });
+	next({ status: 404, message: 'page not found' });
 });
 
 app.use(handle400);
