44b055b625fdb258e80e6d8cd9f8e8166ba1c682
Jake <heaney_jake@outlook.com>
2019-01-08 15:49:12 +0000

    minor update

--- controllers/usersContoller.js
+++ controllers/usersContoller.js
@@ -1,22 +1,22 @@
 const connection = require('../db/connection');
 
 exports.getAllUsers = (req, res, next) => {
-  connection('users')
-    .select('*')
-    .then((users) => {
-      res.status(200).send({ users });
-    })
-    .catch(next);
+	connection('users')
+		.select('*')
+		.then((users) => {
+			res.status(200).send({ users });
+		})
+		.catch(next);
 };
 
 exports.fetchSpecificUser = (req, res, next) => {
-  const { user_id } = req.params;
-  connection('users')
-    .select('*')
-    .where({ 'users.user_id': user_id })
-    .then(([user]) => {
-      if (!user) return Promise.reject({ status: 404, message: 'page not found' });
-      return res.status(200).send({ user });
-    })
-    .catch(next);
+	const { username } = req.params;
+	connection('users')
+		.select('*')
+		.where({ 'users.username': username })
+		.then(([ user ]) => {
+			if (!user) return Promise.reject({ status: 404, message: 'page not found' });
+			return res.status(200).send({ user });
+		})
+		.catch(next);
 };
--- routers/usersRouters.js
+++ routers/usersRouters.js
@@ -1,16 +1,9 @@
 const usersRouter = require('express').Router();
 const { handle405 } = require('../errors');
 
-
 const { getAllUsers, fetchSpecificUser } = require('../controllers/usersContoller');
 
-usersRouter
-  .route('/')
-  .get(getAllUsers)
-  .all(handle405);
-usersRouter
-  .route('/:user_id')
-  .get(fetchSpecificUser)
-  .all(handle405);
+usersRouter.route('/').get(getAllUsers).all(handle405);
+usersRouter.route('/:username').get(fetchSpecificUser).all(handle405);
 
 module.exports = usersRouter;
