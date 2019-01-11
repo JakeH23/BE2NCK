e1ac38695a8e50deccb5a8dd4a7c47d9fb67b4a9
Jake <heaney_jake@outlook.com>
2019-01-11 09:56:54 +0000

    minor update 4

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,34 +1,18 @@
-const {
-  topicData,
-  userData,
-  articleData,
-  commentData,
-} = require('../db/data/test-data');
-const {
-  userLookup, formatArticles, articleLookup, formatComments,
-} = require('../db/data/utils');
+const { topicData, userData, articleData, commentData } = require('../db/data/development-data');
+const { userLookup, formatArticles, articleLookup, formatComments } = require('../db/data/utils');
 
-exports.seed = function (knex, Promise) {
-  return Promise.all([
-    knex('topics').del(),
-    knex('users').del(),
-    knex('articles').del(),
-    knex('comments').del(),
-  ])
-    .then(() => knex('topics')
-      .insert(topicData)
-      .returning('*'))
-    .then(() => knex('users')
-      .insert(userData)
-      .returning('*'))
-    .then((usersRows) => {
-      const userLookObj = userLookup(usersRows);
-      const formattedArticles = formatArticles(articleData, userLookObj);
-      return Promise.all([userLookObj, knex('articles').insert(formattedArticles).returning('*')]);
-    })
-    .then(([userLookObj, articleRows]) => {
-      const articleLookupObj = articleLookup(articleRows);
-      const formattedComments = formatComments(commentData, articleLookupObj, userLookObj);
-      return knex('comments').insert(formattedComments).returning('*');
-    });
+exports.seed = function(knex, Promise) {
+	return Promise.all([ knex('topics').del(), knex('users').del(), knex('articles').del(), knex('comments').del() ])
+		.then(() => knex('topics').insert(topicData).returning('*'))
+		.then(() => knex('users').insert(userData).returning('*'))
+		.then((usersRows) => {
+			const userLookObj = userLookup(usersRows);
+			const formattedArticles = formatArticles(articleData, userLookObj);
+			return Promise.all([ userLookObj, knex('articles').insert(formattedArticles).returning('*') ]);
+		})
+		.then(([ userLookObj, articleRows ]) => {
+			const articleLookupObj = articleLookup(articleRows);
+			const formattedComments = formatComments(commentData, articleLookupObj, userLookObj);
+			return knex('comments').insert(formattedComments).returning('*');
+		});
 };
